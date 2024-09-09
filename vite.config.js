ea82e86a163c88288f0644993591d0c4f2969e33
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-09-09 20:23:01 +0200

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    base: "/DSA-binary-search-dictionary/",
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name]-[hash].js',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash].[ext]'
+            }
+        }
+    }
+});
